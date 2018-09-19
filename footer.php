ce71758209981df621c12fc90859cd3eb04302ce
Bianca <biaamesquitaa@gmail.com>
2018-09-19 00:54:36 +0000

    detalhezinho

---
+++ footer.php
@@ -0,0 +1,9 @@
+  <div class="row">
+     <div class="col-md-12 rodape"
+        <p>Site Desenvolvido por: 3/5 Upside (Bianca, Bruno, Nicolas)</p>
+        </div>
+    </div> 
+        <?php wp_footer();?>
+        
+    </body>
+</html>
